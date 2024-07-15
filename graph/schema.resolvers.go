baddf1a7dd8798d16101f51cd59a128a3049d54c
Matthew Hope <chickazama@gmail.com>
2024-07-15 18:17:55 +0100

    Resolve Links query function

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -33,7 +33,14 @@ func (r *mutationResolver) RefreshToken(ctx context.Context, input model.Refresh
 
 // Links is the resolver for the links field.
 func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
-	panic(fmt.Errorf("not implemented: Links - links"))
+	var ret []*model.Link
+	dummy := &model.Link{
+		Title:   "Dummy Link",
+		Address: "https://example.com",
+		User:    &model.User{Name: "Matty"},
+	}
+	ret = append(ret, dummy)
+	return ret, nil
 }
 
 // Mutation returns MutationResolver implementation.
