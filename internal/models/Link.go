6c3d0c7ca92389da87fe9b80365b9e6a408f191b
Matthew Hope <chickazama@gmail.com>
2024-07-15 18:50:57 +0100

    Update Link model to store UserID field - not User field specifically

--- internal/models/Link.go
+++ internal/models/Link.go
@@ -6,6 +6,29 @@ type Link struct {
 	ID      string
 	Title   string
 	Address string
-	User    *User
+	UserID  string
 	db      *sql.DB
+}
+
+func (l *Link) Save() (int64, error) {
+	query := `INSERT INTO LINKS (
+		ID,
+		Title,
+		Address,
+		UserID
+	) VALUES (?, ?, ?, ?);`
+	stmt, err := l.db.Prepare(query)
+	if err != nil {
+		return 0, err
+	}
+	defer stmt.Close()
+	res, err := stmt.Exec(l.ID, l.Title, l.Address, l.UserID)
+	if err != nil {
+		return 0, err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+	return id, err
 }
